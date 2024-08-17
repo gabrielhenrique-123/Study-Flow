0733cd4ef027d172f9f419ea50abc6c401d54d2a
Gabriel Rocha <gabrielherniquerocha@gmail.com>
2024-08-17 17:14:19 -0300

    Tabs added

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen(){
+  return(
+    <View style = {styles.container}><Text>Study Flow</Text><StatusBar style="auto"/></View>
+  );
+}
+
+function CalendarScreen(){
+  return(
+    <View style = {styles.container}><Text>Calendar time</Text><StatusBar style="auto"/></View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Lets begin!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer><Tab.Navigator><Tab.Screen name="Home" component={HomeScreen} /><Tab.Screen name="My Calendar" component={CalendarScreen} /></Tab.Navigator></NavigationContainer>
   );
+  
 }
 
 const styles = StyleSheet.create({
