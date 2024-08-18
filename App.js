b4b5c0b3aac141291a99077f4d278387ee7eb938
Gabriel Rocha <gabrielherniquerocha@gmail.com>
2024-08-18 20:54:16 -0300

    Home is done

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 const Tab = createBottomTabNavigator();
 
-function HomeScreen(){
+const Stack = createStackNavigator();
+
+function HomeScreen({navigation}){
   return(
-    <View style = {styles.container}><Text>Study Flow</Text><StatusBar style="auto"/></View>
+    <SafeAreaView style = {styles.container}>
+      <Text marginLeft = {17} style = {styles.titleText}>Bem vindo, Aluno!</Text>
+      <View style = {styles.datesButton}>
+        <TouchableOpacity backgroundColor='brown' style = {styles.button}>
+          <Text style = {styles.buttonText}>Provas</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style = {styles.button}>
+          <Text style = {styles.buttonText}>Trabalhos</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style = {styles.button}>
+          <Text style = {styles.buttonText}>Horário</Text>
+        </TouchableOpacity>
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+    </View>
   );
 }
 
@@ -20,7 +48,12 @@ function CalendarScreen(){
 
 export default function App() {
   return (
-    <NavigationContainer><Tab.Navigator><Tab.Screen name="Home" component={HomeScreen} /><Tab.Screen name="My Calendar" component={CalendarScreen} /></Tab.Navigator></NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="My Calendar" component={CalendarScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
   
 }
@@ -28,8 +61,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+
+  datesButton: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  titleText: {
+    fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: 'green',
+    paddingVertical: 15, // Altura (espaçamento interno)
+    paddingHorizontal: 30, // Largura (espaçamento interno)
+    borderRadius: 5, // Bordas arredondadas
+    marginVertical: 10, // Espaçamento
+    width: 300,
+    height: 100,
+    justifyContent: 'center',  
+  },
+
+  buttonText: {
+    color: 'white', // Cor do texto do botão
+    fontSize: 18, // Tamanho do texto
+    alignSelf: 'center',
+  }
 });
